03da6670f6653013d95aeac3502c567b9d383045
D_Zucho <danny.nico.dev@outlook.com>
2021-12-27 03:12:00 -0800

    start routing example and layout

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,25 @@
 import Link from 'next/link'
+import Head from 'next/head'
+import Script from 'next/script'
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+        {/* Any REGULAR html script tag for
+            JS that would need to be loaded as soon as possible
+            is added in the Head as well
+         */}
+      </Head>
+      <Script
+        src='https://connect.facebook.net/en_US/sdk.js'
+        strategy='lazyOnload'
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
+
       <h1>First Post</h1>
       <h2>
         <Link href='/'>
